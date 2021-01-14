03d2dd506a1a6143c027dab6d79738030f0317b0
pandaXiong01 <1159508433@qq.com>
2021-01-14 10:10:55 +0800

    button组件测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from './components/Button'
+import './styles/index.scss'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="button" size='sm' btnType='danger'>按钮</Button>
       </header>
     </div>
   );
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -23,7 +23,7 @@ export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
  * ### 引用方法
  * 
  * ~~~js
- * import { Button } from 'vikingship'
+ * import { Button } from 'panda-design'
  * ~~~
  */
 export const Button: FC<ButtonProps> = (props) => {
